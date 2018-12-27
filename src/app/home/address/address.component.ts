9d0a25386fb5679e28a98a4fbe6b7558ff866c1f
lizhihao97 <940166841@qq.com>
2018-12-27 18:30:44 +0800

    complete home child component

--- src/app/home/address/address.component.ts
+++ src/app/home/address/address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-address',
@@ -6,10 +7,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.scss']
 })
 export class AddressComponent implements OnInit {
-
-  constructor() { }
+  tplModal: NzModalRef;
+  isVisible = false;
+  curAddressEdit;
+  curAddress;
+  intputAddress = {
+    receiver_name: '',
+    receiver_phone: '',
+    receiver_province: '',
+    receiver_city: '',
+    receiver_district: '',
+    receiver_address: '',
+    receiver_zip: ''
+  };
+  data = [
+    {
+      receiver_name: '李祉昊',
+      receiver_phone: '13868224404',
+      receiver_province: '浙江省',
+      receiver_city: '舟山市',
+      receiver_district: '普陀区',
+      receiver_address: 'xxx路xxx',
+      receiver_zip: '316100'
+    },
+    {
+      receiver_name: '狗篮子',
+      receiver_phone: '120',
+      receiver_province: '浙江省',
+      receiver_city: '舟山市',
+      receiver_district: '普陀区',
+      receiver_address: 'xxx路xxx',
+      receiver_zip: '316100'
+    }
+  ];
+  constructor(private modalService: NzModalService) { }
 
   ngOnInit() {
   }
 
+  craeteEditModal(editModal, curAddress) {
+    this.curAddress = curAddress;
+    this.curAddressEdit = {...curAddress};
+    console.log(this.curAddress);
+    this.modalService.create({
+      nzTitle: '编辑地址',
+      nzContent: editModal,
+      nzClosable: false,
+      nzOnOk: () => {
+        this.curAddress['receiver_name'] = this.curAddressEdit['receiver_name'];
+        this.curAddress['receiver_phone'] = this.curAddressEdit['receiver_phone'];
+        this.curAddress['receiver_province'] = this.curAddressEdit['receiver_province'];
+        this.curAddress['receiver_city'] = this.curAddressEdit['receiver_city'];
+        this.curAddress['receiver_district'] = this.curAddressEdit['receiver_district'];
+        this.curAddress['receiver_address'] = this.curAddressEdit['receiver_address'];
+        this.curAddress['receiver_zip'] = this.curAddressEdit['receiver_zip'];
+      }
+    });
+  }
+
+  createAddModal(addModal) {
+    this.modalService.create({
+      nzTitle: '设置地址',
+      nzContent: addModal,
+      nzClosable: false,
+      nzOnOk: () => {
+        this.data.push(this.intputAddress);
+        this.data = [...this.data];
+        this.intputAddress = {
+          receiver_name: '',
+          receiver_phone: '',
+          receiver_province: '',
+          receiver_city: '',
+          receiver_district: '',
+          receiver_address: '',
+          receiver_zip: ''
+        };
+        this.isVisible = false;
+      }
+    });
+  }
 }
--- src/app/home/shopping-cart/shopping-cart.component.ts
+++ src/app/home/shopping-cart/shopping-cart.component.ts
@@ -6,10 +6,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
+  allChecked = false;
+  indeterminate = false;
+  displayData = [];
+  totalPrice: number;
+  data = [
+    {
+      pid: 1,
+      name: '百合花',
+      price: 299,
+      quantity: 1,
+      create_time: '2018-12-26',
+      checked: false
+    },
+    {
+      pid: 1,
+      name: '菊花',
+      price: 188,
+      quantity: 1,
+      create_time: '2018-12-26',
+      checked: false
+    },
+    {
+      pid: 1,
+      name: '水仙花',
+      price: 199,
+      quantity: 1,
+      create_time: '2018-12-26',
+      checked: false
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  currentPageDataChange($event: Array<{ pid: number;
+    name: string; price: number; quantity: number; create_time: string; checked: boolean; }>): void {
+    this.displayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    const allChecked = this.displayData.filter(value => !value.disabled).every(value => value.checked === true);
+    const allUnChecked = this.displayData.filter(value => !value.disabled).every(value => !value.checked);
+    this.allChecked = allChecked;
+    this.indeterminate = (!allChecked) && (!allUnChecked);
+    this.calTotalPrice();
+  }
+
+  checkAll(value: boolean): void {
+    this.displayData.forEach(data => {
+      if (!data.disabled) {
+        data.checked = value;
+      }
+    });
+    this.refreshStatus();
+  }
+
+  reduce(data) {
+    if (data.quantity > 1) {
+      data.quantity -= 1;
+    }
+    this.refreshStatus();
+  }
+
+  add(data) {
+    if (data.quantity < 99) {
+      data.quantity += 1;
+    }
+    this.refreshStatus();
+  }
+
+  calTotalPrice() {
+    const checkData = [...this.displayData].filter(item => item.checked === true);
+    console.log(checkData);
+
+    if (checkData.length) {
+      let totalPrice = 0;
+      for (const data of checkData) {
+        totalPrice += data.quantity * data.price;
+      }
+      this.totalPrice = totalPrice;
+    }
   }
 
 }
